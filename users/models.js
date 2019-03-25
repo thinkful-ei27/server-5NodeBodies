e78bbbbc78b8657fba5d48bb176f7f2740bf934d
Jordan Heffernan <jheff2@gmail.com>
2019-03-25 14:43:49 -0700

    fixed login bug

--- users/models.js
+++ users/models.js
@@ -14,26 +14,27 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  firstName: {type: String, default: ''},
-  lastName: {type: String, default: ''}
+  name: { type: String, default: '' },
+  adventures: []
 });
 
-UserSchema.methods.serialize = function() {
+UserSchema.methods.serialize = function () {
   return {
+    userId: this._id || '',
     username: this.username || '',
-    firstName: this.firstName || '',
-    lastName: this.lastName || ''
+    name: this.name || '',
+    adventures: this.adventures || []
   };
 };
 
-UserSchema.methods.validatePassword = function(password) {
+UserSchema.methods.validatePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-UserSchema.statics.hashPassword = function(password) {
+UserSchema.statics.hashPassword = function (password) {
   return bcrypt.hash(password, 10);
 };
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = { User };
