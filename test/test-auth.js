6ff0b14f4299adf42f70e841d8e73e981c03e7b6
David <dajohnson1998@gmail.com>
2019-03-26 10:43:41 -0400

    commiting un added changes

--- test/test-auth.js
+++ test/test-auth.js
@@ -5,7 +5,7 @@ const chaiHttp = require('chai-http');
 const jwt = require('jsonwebtoken');
 
 const { app, runServer, closeServer } = require('../server');
-const { User } = require('../users');
+const { User } = require('../models/userModel');
 const { JWT_SECRET, TEST_DATABASE_URL } = require('../config');
 
 const expect = chai.expect;
